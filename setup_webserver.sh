9e69ccc1f600bfecc9cf72869093163c5e184a67
Emil Moe <emil@moegroup.dk>
2021-04-09 19:34:39 +0200

    Update setup_webserver.sh

--- setup_webserver.sh
+++ setup_webserver.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-if [ ! -f "~/.cloudmonitor/setup_webserver" ]
+if [ ! -f "~/.cabin/setup_webserver" ]
 then
   mkdir ~/traefik-proxy && cd ~/traefik-proxy
   wget https://raw.githubusercontent.com/EmilMoe/traefik-proxy/main/docker-compose.yml
   docker-compose up -d
 
-  echo true > ~/.cloudmonitor/setup_webserver
+  echo true > ~/.cabin/setup_webserver
 fi
