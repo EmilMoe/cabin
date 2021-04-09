70681debbb52c6fd710a77fa2ec091c1d98d092b
Emil Moe <emil@moegroup.dk>
2021-04-09 18:53:47 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ wget https://github.com/EmilMoe/clomo/archive/refs/heads/main.zip
 apt-get install unzip -yq
 unzip main.zip && rm main.zip
 chmod +x init.sh setup_webserver.sh
+mv como /usr/local/bin/como && chmod +x /usr/local/bin/como
