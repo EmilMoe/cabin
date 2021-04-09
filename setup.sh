81cdf61bd18e519417d5918aa04c58e1cc2bb5e9
Emil Moe <emil@moegroup.dk>
2021-04-09 18:59:39 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 mkdir -p ~/.cloudmonitor && cd ~/.cloudmonitor
 wget https://github.com/EmilMoe/clomo/archive/refs/heads/main.zip
 apt-get install unzip -yq
-unzip main.zip && rm main.zip
+unzip main.zip . && rm main.zip
 chmod +x init.sh setup_webserver.sh
 mv clomo /usr/local/bin/clomo && chmod +x /usr/local/bin/clomo
 sh init.sh
