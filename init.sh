c2fced05a3f37595a1cd69d0c97692b1c601bba1
Emil Moe <emil@moegroup.dk>
2021-04-09 18:02:57 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,5 @@
 apt-get update
 apt-get upgrade -yq
 apt-get install wget docker.io -yq
-mkdir ~/traefik-proxy && cd ~/traefik-proxy
-wget https://raw.githubusercontent.com/EmilMoe/traefik-proxy/main/docker-compose.yml
-docker-compose up -d
 
 echo true > /initialized
