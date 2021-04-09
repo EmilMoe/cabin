9948a539b93ad0df9f289e22f37708dac8691306
Emil Moe <emil@moegroup.dk>
2021-04-09 18:12:03 +0200

    Update setup_webserver.sh

--- setup_webserver.sh
+++ setup_webserver.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-mkdir ~/traefik-proxy && cd ~/traefik-proxy
-wget https://raw.githubusercontent.com/EmilMoe/traefik-proxy/main/docker-compose.yml
-docker-compose up -d
+if [ ! -f "~/.cloudmonitor/initialized" ]
+then
+  mkdir ~/traefik-proxy && cd ~/traefik-proxy
+  wget https://raw.githubusercontent.com/EmilMoe/traefik-proxy/main/docker-compose.yml
+  docker-compose up -d
 
-echo true > ~/.cloudmonitor/setup_webserver
+  echo true > ~/.cloudmonitor/setup_webserver
+fi
