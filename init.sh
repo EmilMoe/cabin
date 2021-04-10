4dc30f698152f41f5d34ee837d81a8ade8cf0b5d
Emil Moe <emil@moegroup.dk>
2021-04-10 15:24:50 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ if [ ! -f "~/.cabin/initialized" ]
 then
   apt-get update
   apt-get upgrade -yq
-  apt-get install docker.io -yq
+  apt-get install docker.io docker-compose -yq
 
   echo true > ~/.cabin/initialized
 fi
