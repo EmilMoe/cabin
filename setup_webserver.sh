85c2f3cdaf348731884f659f86061a4b736b45f5
Emil Moe <emil@moegroup.dk>
2021-04-10 10:40:16 +0200

    Update setup_webserver.sh

--- setup_webserver.sh
+++ setup_webserver.sh
@@ -2,9 +2,11 @@
 
 if [ ! -f "~/.cabin/setup_webserver" ]
 then
-  mkdir ~/traefik-proxy && cd ~/traefik-proxy
+  CURRENT_DIR=${pwd}
+  mkdir ~/.cabin/traefik-proxy && cd ~/.cabin/traefik-proxy
   wget https://raw.githubusercontent.com/EmilMoe/traefik-proxy/main/docker-compose.yml
   docker-compose up -d
+  cd ${CURRENT_DIR}
 
   echo true > ~/.cabin/setup_webserver
 fi
